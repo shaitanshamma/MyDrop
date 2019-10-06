1aa81216549d2455d53c5fb35b535b5789d74120
Andrey Kurbatov <kurbatov_andr@hotmail.com>
2019-10-06 17:24:16 +0300

    Проба добавить окно сервера

--- client/src/main/java/NettyNetwork.java
+++ client/src/main/java/NettyNetwork.java
@@ -43,7 +43,7 @@ public class NettyNetwork {
             clientBootstrap.handler(new ChannelInitializer<SocketChannel>() {
                 protected void initChannel(SocketChannel socketChannel) throws Exception {
                     socketChannel.pipeline().addLast(new ObjectDecoder(50 * 1024 * 1024, ClassResolvers.cacheDisabled(null)),
-                            new ObjectEncoder(), new ClientDownload(cloudWindowController,logOnWindowController));
+                            new ObjectEncoder(), new ClientHandler(cloudWindowController,logOnWindowController));
                     currentChannel = socketChannel;
 
                 }
